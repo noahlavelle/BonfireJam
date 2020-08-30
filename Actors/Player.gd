501ea50d259366efc314b207e5ff19766aa67c1b
NoahLavelle <39242025+NoahLavelle@users.noreply.github.com>
2020-08-30 15:32:41 +0100

    Fixed l6

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -25,7 +25,7 @@ func _physics_process(_delta):
 func getMoveVector():
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("move_jump") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("move_jump") and ((is_on_floor() and gravity > 0) or (is_on_ceiling() and gravity < 0)) else 1.0
 		)
 
 func calculate_move_velocity(
@@ -39,16 +39,21 @@ func calculate_move_velocity(
 	return out
 
 func handleAnimations(moveVector):
-	if moveVector.y != 0 and not is_on_floor():
+	var mod
+	if gravity > 0:
+		mod = -1
+	else:
+		mod = 1
+	if moveVector.y != 0 and not ((is_on_floor() and gravity > 0) or (is_on_ceiling() and gravity < 0)):
 		$AnimatedSprite.animation = "jump"
 		if moveVector.x != 0:
-			$AnimatedSprite.flip_h = clamp(moveVector.x * -1, 0, 1)
+			$AnimatedSprite.flip_h = clamp(moveVector.x * mod, 0, 1)
 		return
 	if moveVector.x == 0:
 		$AnimatedSprite.animation = "idle"
 	else:
 		$AnimatedSprite.animation = "run"
-		$AnimatedSprite.flip_h = clamp(moveVector.x * -1, 0, 1)
+		$AnimatedSprite.flip_h = clamp(moveVector.x * mod, 0, 1)
 
 func restart():
 	get_parent().get_node("WinBox/AnimationPlayer").play("TransOut")
